f4720880cf4ef43b40e1db1d156fe7e82f7a8870
Peter Marshall <peter.marshall@ocsbstudent.ca>
2021-03-04 23:54:28 -0500

    Fix style

--- sw.js
+++ sw.js
@@ -14,17 +14,17 @@ const cachedFiles = [
   '/sw.js'
 ]
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(cacheName).then(function (cache) {
       return cache.addAll(cachedFiles);
     })
   );
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
+    caches.match(e.request).then(function (response) {
       return response || fetch(e.request);
     })
   );
