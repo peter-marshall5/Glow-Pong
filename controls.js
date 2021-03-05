4f1f746956b4bf92a2a56da9dd4301057e6f8462
Peter Marshall <peter.marshall@ocsbstudent.ca>
2021-03-05 00:01:16 -0500

    Remove unnecessary code

--- controls.js
+++ controls.js
@@ -100,7 +100,7 @@ function onclick (e) {
 const touchMargin = 60
 
 function getTouchOffset (pageX, pageY) {
-  const rect = window.canvas.getBoundingClientRect();
+  const rect = window.canvas.getBoundingClientRect()
   return { x: pageX - rect.left, y: pageY - rect.top }
 }
 
@@ -128,12 +128,12 @@ function handleTouch (e) {
   if (t.l) {
     window.leftPaddle.move(null, Math.max(window.topWall,
       Math.min(window.bottomWall - window.leftPaddle.size.h,
-      t.l - window.leftPaddle.size.h / 2)))
+        t.l - window.leftPaddle.size.h / 2)))
   }
   if (t.r && window.getGameMode() !== 'bot') {
     window.rightPaddle.move(null, Math.max(window.topWall,
       Math.min(window.bottomWall - window.rightPaddle.size.h,
-      t.r - window.rightPaddle.size.h / 2)))
+        t.r - window.rightPaddle.size.h / 2)))
   }
 }
 
@@ -145,19 +145,8 @@ function ontouchmove (e) {
   handleTouch(e)
 }
 
-function ontouchend (e) {
-  const t = processTouch(e)
-  if (!t.l) {
-    touches.l = null
-  }
-  if (!t.r) {
-    touches.r = null
-  }
-}
-
 window.addEventListener('keydown', onkeydown)
 window.addEventListener('keyup', onkeyup)
 window.canvas.addEventListener('click', onclick)
 window.canvas.addEventListener('touchstart', ontouchstart)
 window.canvas.addEventListener('touchmove', ontouchmove)
-window.canvas.addEventListener('touchend', ontouchend)
--- sw.js
+++ sw.js
@@ -17,15 +17,15 @@ const cachedFiles = [
 self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(cacheName).then(function (cache) {
-      return cache.addAll(cachedFiles);
+      return cache.addAll(cachedFiles)
     })
-  );
-});
+  )
+})
 
 self.addEventListener('fetch', function (e) {
   e.respondWith(
     caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
+      return response || fetch(e.request)
     })
-  );
-});
+  )
+})
